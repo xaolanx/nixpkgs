03447df522a8f9e936e1bcc9b5ea523eda173faa
Xaolan <ah0199004@gmail.com>
2025-08-01 13:41:59 +0800

    intel-media-sdk

--- pkgs/by-name/in/intel-media-sdk/package.nix
+++ pkgs/by-name/in/intel-media-sdk/package.nix
@@ -17,12 +17,12 @@
 
 stdenv.mkDerivation rec {
   pname = "intel-media-sdk";
-  version = "23.2.2";
+  version = "23.2.2-1";
 
   src = fetchFromGitHub {
     owner = "Intel-Media-SDK";
     repo = "MediaSDK";
-    rev = "intel-mediasdk-${version}";
+    rev = "intel-mediasdk-23.2.2";
     hash = "sha256-wno3a/ZSKvgHvZiiJ0Gq9GlrEbfHCizkrSiHD6k/Loo=";
   };
 
@@ -57,7 +57,10 @@ stdenv.mkDerivation rec {
     "-DBUILD_SAMPLES=OFF"
     "-DBUILD_TESTS=${if doCheck then "ON" else "OFF"}"
     "-DUSE_SYSTEM_GTEST=ON"
+    "-DCMAKE_CXX_STANDARD=17"
   ];
+
+  env.NIX_CFLAGS_COMPILE = "-std=c++17";
 
   doCheck = true;
 
